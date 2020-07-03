ad32eaef23ed96533e48b78ed19c980e32ac2802
Juan Javier Cabanas <juanjavier.cabanas@nc43tech.com>
2020-07-03 21:49:12 +0200

    OkHttpClient Wrapper created

---
+++ network/src/main/java/me/javicabanas/randomuser/network/client/Client.kt
@@ -0,0 +1,29 @@
+package me.javicabanas.randomuser.network.client
+
+import me.javicabanas.randomuser.network.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
+
+private const val DEFAULT_TIMEOUT_SECONDS: Long = 20
+private const val WEBSOCKET_PING_INTERVAL_SECONDS: Long = 10
+private const val DEFAULT_READ_TIMEOUT_SECONDS: Long = 45
+private const val NO_TIMEOUT_SECONDS: Long = 0
+
+class HttpClient {
+    private val logInterceptor by lazy {
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+    val okClient: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .connectTimeout(DEFAULT_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .readTimeout(DEFAULT_READ_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .writeTimeout(NO_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .pingInterval(WEBSOCKET_PING_INTERVAL_SECONDS, TimeUnit.SECONDS)
+            .addLoggingInterceptor(logInterceptor)
+            .build()
+    }
+
+    private fun OkHttpClient.Builder.addLoggingInterceptor(loggingInterceptor: HttpLoggingInterceptor) =
+        apply { if (BuildConfig.DEBUG) addInterceptor(loggingInterceptor) }
+}
