2df8571bd913559edd9213816c005ed02c4b542b
Juan Javier Cabanas <juanjavier.cabanas@nc43tech.com>
2020-07-05 18:56:43 +0200

    inject GetUser constructor

--- feature/usercatalog/domain/src/main/java/me/javicabanas/randomuser/usercatalog/domain/GetUser.kt
+++ feature/usercatalog/domain/src/main/java/me/javicabanas/randomuser/usercatalog/domain/GetUser.kt
@@ -5,8 +5,10 @@ import me.javicabanas.randomuser.core.failure.Failure
 import me.javicabanas.randomuser.core.functional.Either
 import me.javicabanas.randomuser.core.model.User
 import me.javicabanas.randomuser.usercatalog.domain.data.UserRepository
+import javax.inject.Inject
 
-class GetUser(private val userRepository: UserRepository) : BaseInteractor<String, User>() {
+class GetUser @Inject constructor(private val userRepository: UserRepository) :
+    BaseInteractor<String, User>() {
     override suspend fun run(request: String): Either<Failure, User> =
         userRepository.getUser(request)
 }
