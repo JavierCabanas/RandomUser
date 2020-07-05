22476f509eb0f1ab4782ec8175f1667e2bd4d901
Juan Javier Cabanas <juanjavier.cabanas@nc43tech.com>
2020-07-06 00:34:04 +0200

    test viewmodel

--- feature/usercatalog/src/test/java/me/javicabanas/randomuser/usercatalog/userdetail/UserDetailViewModelTest.kt
+++ feature/usercatalog/src/test/java/me/javicabanas/randomuser/usercatalog/userdetail/UserDetailViewModelTest.kt
@@ -1,4 +1,4 @@
-package me.javicabanas.randomuser.usercatalog.userlist
+package me.javicabanas.randomuser.usercatalog.userdetail
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
@@ -9,9 +9,10 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import me.javicabanas.randomuser.core.failure.Failure
 import me.javicabanas.randomuser.core.functional.toLeft
 import me.javicabanas.randomuser.core.functional.toRight
+import me.javicabanas.randomuser.core.model.User
 import me.javicabanas.randomuser.testcommons.CoroutinesTestRule
 import me.javicabanas.randomuser.testcommons.UserMother
-import me.javicabanas.randomuser.usercatalog.domain.GetAllUsers
+import me.javicabanas.randomuser.usercatalog.domain.GetUser
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -19,19 +20,19 @@ import org.junit.Test
 import org.junit.rules.TestRule
 
 @ExperimentalCoroutinesApi
-class UserListViewModelTest {
+class UserDetailViewModelTest {
     @get:Rule
     val coroutinesRule: TestRule = CoroutinesTestRule()
 
     @get:Rule
     val liveDataRule: TestRule = InstantTaskExecutorRule()
-    private val getAllUsers: GetAllUsers = mockk()
-    private lateinit var viewModel: UserListViewModel
-    private val mockObserver: Observer<UserListViewState> = mockk(relaxed = true)
+    private val getUser: GetUser = mockk()
+    private lateinit var viewModel: UserDetailViewModel
+    private val mockObserver: Observer<UserDetailViewState> = mockk(relaxed = true)
 
     @Before
     fun setupViewModel() {
-        viewModel = UserListViewModel(getAllUsers)
+        viewModel = UserDetailViewModel(getUser)
         viewModel.viewState.observeForever(mockObserver)
     }
 
@@ -42,33 +43,39 @@ class UserListViewModelTest {
 
     @Test
     fun `should lead to WithData state when users are retrieved`() {
-        val users = givenAListWithUsers()
-        viewModel.loadUsers()
-        assert(viewModel.viewState.value == UserListViewState.WithData(users))
+        val user = givenAUser()
+        val uiModel = user.toDetailUi()
+        viewModel.loadUser(user.id)
+        assert(viewModel.viewState.value == UserDetailViewState.WithData(uiModel))
     }
 
-    private fun givenAListWithUsers(): List<UserUiItem> {
-        val users = UserMother.users
-        coEvery { getAllUsers(Unit) } returns users.toRight()
-        return users.map { it.toListUi() }
+    private fun givenAUser(): User {
+        val user = UserMother.user
+        coEvery { getUser(user.id) } returns user.toRight()
+        return user
     }
 
     @Test
     fun `should lead to Error state when a failure is received`() {
         givenAFailureResponse()
-        viewModel.loadUsers()
-        assert(viewModel.viewState.value == UserListViewState.Error)
+        viewModel.loadUser(UserMother.user.id)
+        assert(viewModel.viewState.value == UserDetailViewState.Error)
     }
 
     private fun givenAFailureResponse() {
-        coEvery { getAllUsers(Unit) } returns Failure.ElementNotFound("").toLeft()
+        coEvery { getUser(any()) } returns Failure.ElementNotFound("").toLeft()
     }
 
     @Test
     fun `loading state is setted while getting users`() {
-        givenAListWithUsers()
-        viewModel.loadUsers()
-        verify { mockObserver.onChanged(UserListViewState.Loading) }
+        val user = givenAUser()
+        viewModel.loadUser(user.id)
+        verify { mockObserver.onChanged(UserDetailViewState.Loading) }
     }
 
+    @Test
+    fun `should lead to Error state when request id is null`() {
+        viewModel.loadUser(null)
+        assert(viewModel.viewState.value == UserDetailViewState.Error)
+    }
 }
